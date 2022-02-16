302a913d8de31d31a3e06689975d6d0946d63f70
FernandoZap <fernando9az@gmail.com>
2022-02-16 19:04:47 -0300

    atualizando html usuario de sessao

--- core/views.py
+++ core/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 def home(request):
 	usuario = ''
-	return render (request, 'core/home.html', { 'usuario': usuario })
+	user_name = request.user.username
+	return render (request, 'core/home.html', { 'usuario': user_name })
+
+
+def sessao(request):
+    if not request.session.get('username'):
+        request.session['username'] = request.user.username
+    return
